15c7cd5a29055507e837bd9c2c5257a62c839c5a
Avraj <avraj@underctrl.io>
2024-06-08 22:45:55 +0300

    implement redis caching

--- api/src/routes/dashboard/@me/index.js
+++ api/src/routes/dashboard/@me/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const { hasPermissions } = require('../../../lib/utils');
+const redis = require('../../../lib/redis');
 
 const router = express.Router();
 
 const DISCORD_ENDPOINT = 'https://discord.com/api/v10';
-const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-const CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
-const REDIRECT_URI = process.env.DISCORD_REDIRECT_URI;
 
 router.get('/', (req, res) => {
   if (req.user) {
@@ -23,6 +21,12 @@ router.get('/guilds', async (req, res) => {
     return res.status(401).json({ message: 'Not logged in' });
   }
 
+  const redisCacheRes = await redis.get(`user-guilds:${req.user.id}`);
+
+  if (redisCacheRes) {
+    return res.status(200).json(JSON.parse(redisCacheRes));
+  }
+
   const guildsRes = await fetch(`${DISCORD_ENDPOINT}/users/@me/guilds`, {
     headers: {
       Authorization: `Bearer ${req.user.accessToken}`,
@@ -37,6 +41,13 @@ router.get('/guilds', async (req, res) => {
 
   const filteredGuilds = guilds.filter((guild) =>
     hasPermissions(guild.permissions, 'ManageGuild')
+  );
+
+  await redis.set(
+    `user-guilds:${req.user.id}`,
+    JSON.stringify(filteredGuilds),
+    'EX',
+    600
   );
 
   res.status(200).json(filteredGuilds);
